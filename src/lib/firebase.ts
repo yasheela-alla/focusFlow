3c21055be429bfe1106475b2680969a7c20d3824
0xAy4sh <yasheela435@gmail.com>
2025-03-09 17:43:30 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,23 +7,19 @@ import {
 } from "firebase/auth";
 
 // Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey:
-    import.meta.env.VITE_FIREBASE_API_KEY ||
-    "AIzaSyBM1W8Gpm3KwDY5YRF0W8q1khOQj1Z_cd0",
-  authDomain:
-    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ||
-    "focusflow-d2728.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "focusflow-d2728",
-  storageBucket:
-    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ||
-    "focusflow-d2728.firebasestorage.app",
-  messagingSenderId:
-    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "384422259398",
-  appId:
-    import.meta.env.VITE_FIREBASE_APP_ID ||
-    "1:384422259398:web:89329335022bcdf93505c7",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+const app = initializeApp(firebaseConfig);
+export default app;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
